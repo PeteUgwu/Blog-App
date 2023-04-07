9f8bf4fc7a45eb95135eadf674ded7f64a245fa0
peter <peterugwu64@gmail.com>
2023-04-07 19:15:17 +0100

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: "User" 
-    has_many :comments
-    has_many :likes
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
 
-    def update_a_user_posts_counter
-        author.increment!(:posts_counter)
-    end
+  def update_a_user_posts_counter
+    author.increment!(:posts_counter)
+  end
 
-    def five_most_recent_comments
-        comments.order("created_at Desc").limit(5)
-    end
+  def five_most_recent_comments
+    comments.order('created_at Desc').limit(5)
+  end
 end
