c5d2c15b960081d9dc7970988b8545fc37c3fb85
peter <peterugwu64@gmail.com>
2023-04-07 19:10:18 +0100

    Insert data, Create models and custom methods

---
+++ app/models/like.rb
@@ -0,0 +1,9 @@
+class Like < ApplicationRecord
+    belongs_to :author, class_name: "User"
+    belongs_to :post
+
+    def update_post_likes_counter
+        post.increment!(:likes_counter)
+    end
+
+end
