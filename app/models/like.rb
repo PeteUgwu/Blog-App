cd3cef852392c6b8df9fdad406e49d7f7eb7b810
peter <peterugwu64@gmail.com>
2023-04-07 19:50:31 +0100

    Implement active callback after_save

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,11 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def update_post_likes_counter
+  after_save :update_likes_counter
+
+  private
+
+  def update_likes_counter
     post.increment!(:likes_counter)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def update_a_user_posts_counter
+  after_save :update_posts_counter
+
+  private
+
+  def update_posts_counter
     author.increment!(:posts_counter)
   end
 
