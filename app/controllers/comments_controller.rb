ec993fea38b3a7b23051711de369c3bf3156303a
peter <peterugwu64@gmail.com>
2023-04-15 19:57:50 +0100

    Fix linters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,10 +7,10 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
 
     if @comment.save
-        redirect_to user_post_path(@comment.post.author, @comment.post)
-     else
-       render :new
-     end
+      redirect_to user_post_path(@comment.post.author, @comment.post)
+    else
+      render :new
+    end
   end
 
   private
